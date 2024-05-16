9377c2538961f2ed1fb47dd02206b509a70d9d5f
Neeraj Kumar <77137361+neerajkrjsr@users.noreply.github.com>
2024-05-16 22:23:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask
-from playwright.sync_api import sync_playwright, Playwright
+import asyncio
+from playwright.async_api import async_playwright, Playwright
 
 app = Flask(__name__)
 
 @app.route('/')
-def run(playwright: Playwright):
+async def run(playwright: Playwright):
     chromium = playwright.chromium
-    browser = chromium.launch()
-    page = browser.new_page()
-    page.goto("https://example.com")
-    browser.close()
+    browser = await chromium.launch()
+    page = await browser.new_page()
+    await page.goto("https://example.com")
+    await page.wait_for_timeout(5000)
+    await browser.close()
 
-with sync_playwright() as playwright:
-    run(playwright)
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+asyncio.run(main())
     
 #def hello_world():
 #    return 'Welcome Neeraj!'
