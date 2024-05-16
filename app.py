d0bb924241847eedd07d6ec3dbfdf757b693cc2a
Neeraj Kumar <77137361+neerajkrjsr@users.noreply.github.com>
2024-05-16 21:15:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from playwright.sync_api import sync_playwright, Playwright
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Welcome Neeraj!'
+def run(playwright: Playwright):
+    firefox = playwright.firefox
+    browser = firefox.launch()
+    page = browser.new_page()
+    page.goto("https://example.com")
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
+    
+#def hello_world():
+#    return 'Welcome Neeraj!'
